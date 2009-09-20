633033599e51ce2e7ebc1c78f490fa86d8578102
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2009-09-20 10:11:23 +0000

    Add link to blog on confirmation of activation
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@16 d0c80317-126d-4d13-a798-19a31160aa05

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -14,7 +14,8 @@ class NewslettersController < ApplicationController
     @newsletter = Newsletter.find_by_activation_key(params[:id])
     if @newsletter
       @newsletter.activate!
-      flash.now[:notice] = "Nous avons validé votre adresse email. Vous recevrez un email dès que SP-Gestion sera disponible."
+      flash.now[:notice] = "Nous avons validé votre adresse email. Vous recevrez un email dès que SP-Gestion sera disponible, " +
+                           "en attendant vous pouvez consulter notre #{link_to("blog", "http://blog.sp-gestion.fr")}."
       render('pages/home')
     else
       redirect_to(home_page_path)
