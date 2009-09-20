b9567a46aa8e7df961b82d59e6ad52b5847f3333
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2009-09-20 09:04:53 +0000

    Add BASE_URL to generate URL in emails and fix for emails (Reference Ticket 37)
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@13 d0c80317-126d-4d13-a798-19a31160aa05

--- app/models/newsletter_mailer.rb
+++ app/models/newsletter_mailer.rb
@@ -3,12 +3,23 @@ class NewsletterMailer < ActionMailer::Base
   def activation_instructions(newsletter)
     setup(newsletter.email)
     subject("Disponibilité de SP-Gestion")
-    body(:newsletter => newsletter)
+
+    content_type    "multipart/alternative"
+
+    part "text/html" do |p|
+      p.body = render_message("activation_instructions.html", :newsletter => newsletter)
+    end
+
+    part "text/plain" do |p|
+      p.body = render_message("activation_instructions.plain", :newsletter => newsletter)
+      p.transfer_encoding = "base64"
+    end
   end  
   
   private
 
   def setup(email)
+    default_url_options[:host] = "www.#{BASE_URL}"
     from("SP-Gestion.fr <pas_de_reponse@sp-gestion.fr>")
     recipients(email)
     sent_on(Time.now)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,3 +26,6 @@ config.action_view.cache_template_loading            = true
 
 # Enable threaded mode
 # config.threadsafe!
+
+# URL
+BASE_URL = "sp-gestion.fr"
