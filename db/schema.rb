30745126476f0b5d79ad2e99ab85d691484e9317
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2009-12-30 21:59:45 +0000

    Drop unused session tables from schema
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@96 d0c80317-126d-4d13-a798-19a31160aa05

--- db/schema.rb
+++ db/schema.rb
@@ -75,15 +75,6 @@ ActiveRecord::Schema.define(:version => 20091220095020) do
     t.datetime "invited_at"
   end
 
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id"
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], :name => "session_id_idx"
-
   create_table "stations", :force => true do |t|
     t.string   "name"
     t.string   "url"
