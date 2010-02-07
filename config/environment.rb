fb83114b77afb05ae78fe25a2d24a5457177db30
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2010-02-07 21:59:46 +0000

    Updated gems and fix for HTML messages
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@137 d0c80317-126d-4d13-a798-19a31160aa05

--- config/environment.rb
+++ config/environment.rb
@@ -45,9 +45,9 @@ Rails::Initializer.run do |config|
   
   config.gem "authlogic",           :version => "2.1.3"
   config.gem "google_analytics",    :version => "1.1.5", :lib => "rubaidh/google_analytics", :source => "http://gems.github.com"
-  config.gem "prawn",               :version => "0.7.1", :lib => "prawn"
-  config.gem "simple-navigation",   :version => "2.2.1", :lib => 'simple_navigation', :source => "http://gemcutter.org/"
-  config.gem "validates_timeliness",:version => "2.2.2"
+  config.gem "prawn",               :version => "0.7.2", :lib => "prawn"
+  config.gem "simple-navigation",   :version => "2.2.2", :lib => 'simple_navigation', :source => "http://gemcutter.org/"
+  config.gem "validates_timeliness",:version => "2.3.0"
   config.gem "will_paginate",       :version => "2.3.12", :source => "http://gemcutter.org/"
 end
 
