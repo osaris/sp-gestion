38cd5ac36a2916590add4b73f2aa3b0b5be18ce9
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2010-02-11 17:45:27 +0000

    Fix for activation error due to expired perishable token
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@140 d0c80317-126d-4d13-a798-19a31160aa05

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -4,7 +4,8 @@ class ConfirmationsController < BackController
   before_filter :require_no_user
 
   def create
-    @user = @station.users.find_using_perishable_token(params[:id])
+    # find without limit on perishable token
+    @user = @station.users.find_using_perishable_token(params[:id], 0)
     if @user.nil? or @user.confirmed?
       flash[:error] = "Compte inexistant ou inactif."
       redirect_to(login_path)
