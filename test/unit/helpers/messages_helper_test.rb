c243c16f404fe0d1672ee615cbcd098f05b4bbdb
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2010-02-17 21:10:44 +0000

    Improve test coverage for messages_helper
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@167 d0c80317-126d-4d13-a798-19a31160aa05

--- test/unit/helpers/messages_helper_test.rb
+++ test/unit/helpers/messages_helper_test.rb
@@ -1,4 +1,25 @@
 require 'test_helper'
 
 class MessagesHelperTest < ActionView::TestCase
+  
+  context "class_tr_message with message read" do
+    setup do
+      @class_tr_message = class_tr_message(Message.new(:read_at => Time.now))
+    end
+
+    should "render read" do
+      assert_equal("read", @class_tr_message)
+    end
+  end
+  
+  context "class_tr_message with message unread" do
+    setup do
+      @class_tr_message = class_tr_message(Message.new(:read_at => nil))
+    end
+
+    should "render unread" do
+      assert_equal("unread", @class_tr_message)
+    end
+  end
+  
 end
