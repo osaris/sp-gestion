83d61a8c002cab7ddb267d237381c6e17d6abf80
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2010-04-18 20:16:16 +0000

    Replace hoptoad by errornot
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@250 d0c80317-126d-4d13-a798-19a31160aa05

---
+++ config/initializers/errornot.rb
@@ -0,0 +1,4 @@
+ErrornotNotifier.configure do |config|
+  config.api_key = 'a898b71a71a90abe8bdb3e29'
+  config.host = 'errors.imagineapp.com'
+end
--- config/initializers/hoptoad.rb
+++
@@ -1,3 +0,0 @@
-HoptoadNotifier.configure do |config|
-  config.api_key = '5744210a487995fefb9436b00be72ecf'
-end
