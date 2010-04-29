e6021fc5230727d890d985e8926445a82163178d
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2010-04-29 20:43:11 +0000

    Fix confirmation process and tests.
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@257 d0c80317-126d-4d13-a798-19a31160aa05

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -12,7 +12,7 @@ class ConfirmationsController < BackController
   end
 
   def update
-    if @user.confirm!(params[:password], password[:password_confirmation])
+    if @user.confirm!(params[:user][:password], params[:user][:password_confirmation])
       flash[:success] = render_to_string(:partial => "welcome") if @user.id == @station.owner_id
       redirect_to(root_back_path)
     else
--- test/functional/confirmations_controller_test.rb
+++ test/functional/confirmations_controller_test.rb
@@ -13,9 +13,9 @@ class ConfirmationsControllerTest < ActionController::TestCase
         @user = @station.users.make(:confirmed)
       end
 
-      context "requesting GET :create with good confirmation code" do
+      context "requesting GET :edit with good confirmation code" do
         setup do
-          get :create, :id => @user.perishable_token
+          get :edit, :id => @user.perishable_token
         end
 
         should_respond_with(:redirect)
@@ -23,25 +23,45 @@ class ConfirmationsControllerTest < ActionController::TestCase
         should_set_the_flash(:error)
       end
     end
-    
+
     context "with an inactive user on this subdomain" do
       setup do
         @user = @station.users.make(:beta)
       end
-      
-      context "requesting GET :create with good confirmation code" do
+
+      context "requesting GET :edit with good confirmation code" do
         setup do
-          get :create, :id => @user.perishable_token
+          get :edit, :id => @user.perishable_token
+        end
+
+        should_respond_with(:success)
+        should_render_template("edit")
+        should_render_with_layout("login")
+      end
+
+      context "requesting POST :update with bad password" do
+        setup do
+          post :update, :id => @user.perishable_token, :user => {:password => '123',
+                                                                 :password_confirmation => ''}
+        end
+
+        should_respond_with(:success)
+        should_render_template("edit")
+        should_render_with_layout("login")
+      end
+
+      context "requesting POST :update with good password" do
+        setup do
+          post :update, :id => @user.perishable_token, :user => {:password => '123456',
+                                                                 :password_confirmation => '123456'}
         end
 
         should_respond_with(:redirect)
         should_redirect_to("back root") { root_back_path }
-        
-        should "be active" do        
+        should "be active" do
           assert(assigns(:user).confirmed?)
-        end        
+        end
         should_be_logged_in
-        should_set_the_flash(:success)
       end
     end
   end
