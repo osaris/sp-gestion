ddf9b41d5499f2565053031e25a071f6478168ec
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2010-05-02 11:43:47 +0000

    Update default uniforms.
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@258 d0c80317-126d-4d13-a798-19a31160aa05

--- app/models/uniform.rb
+++ app/models/uniform.rb
@@ -10,9 +10,11 @@ class Uniform < ActiveRecord::Base
   before_destroy :check_associations
   
   DEFAULTS = [
-    {:title => "Tenue d'intervention", :description => "Casquette, veste F1, pantalon F1, rangers ou bottes."},
-    {:title => "Tenue de feu", :description => "Casque F1, cagoule, veste de feu, gants, ceinturons, surpantalon, rangers ou bottes."},
-    {:title => "Tenue de cérémonie", :description => "Casque F1, veste F1, pantalon F1, rangers ou bottes."}
+    {:title => "Tenue SAP", :description => "Bottes/rangers. Pantalon F1. T-Shirt/polo. (Pull/sweat/veste F1/parka)"},
+    {:title => "Tenue INC", :description => "Bottes/rangers. Pantalon F1. Surpantalon. Chemise F1. Veste de Feu. Ceinturon (avec gants). Casque F1."},
+    {:title => "Tenue SR/DIV", :description => "Bottes/rangers. Pantalon F1. Chemise F1. Veste de Feu. Ceinturon (avec gants). Casque F1 ou F2."},
+    {:title => "Tenue FdF", :description => "Bottes/rangers. Pantalon F2. Chemise F1. Veste de Feu F2. Ceinturon (avec gants). Casque F2."},
+    {:title => "Tenue cérémonie", :description => "Chaussures. Pantalon. Chemise. Gants blancs, képi. (Vareuse)"}
   ]
   
   def self.create_defaults(station)
