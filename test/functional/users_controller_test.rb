d97d5fa44b4c49ca23985169b0ff1e0ea555e4dc
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2010-05-02 16:44:11 +0000

    Improve test coverage for users controller.
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@264 d0c80317-126d-4d13-a798-19a31160aa05

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,8 +1,88 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  setup(:activate_authlogic)
+
+  context "an user logged in and station owner" do
+    setup do
+      @user = User.make(:confirmed)
+      login(Station.make(:owner_id => @user.id), @user)
+    end
+    
+    context "requesting GET :index" do
+      setup do
+        get :index
+      end
+
+      should_respond_with(:success)
+      should_render_template("index")
+      should_render_with_layout("back")
+      
+      should_assign_to(:users)
+    end
+    
+    context "requesting GET :new" do
+      setup do
+        get :new
+      end
+
+      should_respond_with(:success)
+      should_render_template("new")
+      should_render_with_layout("back")
+    end
+    
+    context "requesting POST :create with bad data" do
+      setup do
+        post :create, :user => { :email => '' }
+      end
+    
+      should_respond_with(:success)
+      should_render_template("new")
+      should_render_with_layout("back")
+      
+      should_not_change("number of users") { User.count }
+    end   
+    
+    context "requesting POST :create with good data" do
+      setup do
+        post :create, :user => { :email => 'raphael.emourgeon@gmail.com' }
+      end
+    
+      should_respond_with(:redirect)
+      should_redirect_to("users") { users_path }
+      
+      should_change("number of users", :by => 1) { User.count }
+      should_set_the_flash(:success)
+    end
+    
+    context "requesting DELETE :destroy of account owner (current user)" do
+      setup do
+        delete :destroy, :id => @user.id
+      end
+      
+      should_respond_with(:redirect)
+      should_redirect_to("users") { users_path }
+      
+      should_set_the_flash(:warning)
+    end
+    
+    context "with another user not owner" do
+      setup do
+        @another_user = @station.users.make(:confirmed)
+      end
+
+      context "requesting DELETE :destroy" do
+        setup do
+          delete :destroy, :id => @another_user.id
+        end
+
+        should_respond_with(:redirect)
+        should_redirect_to("users") { users_path }
+
+        should_change("number of users", :by => -1) { User.count }
+        should_set_the_flash(:success)
+      end
+    end
   end
 end
