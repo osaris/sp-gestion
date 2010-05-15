cac3ec426f7465b88e56ef9ed2cb69c90045dcf1
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2010-05-15 09:39:29 +0000

    Add final dot to error messages.
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@281 d0c80317-126d-4d13-a798-19a31160aa05

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,8 @@ class Item < ActiveRecord::Base
 
   belongs_to :check_list
 
-  validates_presence_of :title, :message => "Le titre est obligatoire"
-  validates_numericality_of :quantity, :message => "La quantité doit être un nombre"
+  validates_presence_of :title, :message => "Le titre est obligatoire."
+  validates_numericality_of :quantity, :message => "La quantité doit être un nombre."
   validates_date :expiry, :allow_blank => true, :invalid_date_message => "Format incorrect (JJ/MM/AAAA)"
 
   named_scope :expirings, lambda { |nb_days, station_id|
