11b1fe842d47816d9a0104bbe3a09f7c4c8b12e8
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2010-05-26 20:54:11 +0000

    Roll back to Rails 2.3.5 because accepts_nested_attributes_for seams broken in earlier version.
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@293 d0c80317-126d-4d13-a798-19a31160aa05

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -44,7 +44,7 @@ Rails::Initializer.run do |config|
                                       :secret => "41da19045c1289f045a2797ace137c01908f49d5522ade1da1549022bd40202d663891fbca9dfc4d0f7a092e36ff5b325d38fe6cb22055d843802f37228c1868"}
 
   config.gem "acts-as-taggable-on", :version => "2.0.6"
-  config.gem "authlogic",           :version => "2.1.4"
+  config.gem "authlogic",           :version => "2.1.3"
   config.gem "delayed_job",         :version => "2.0.3"
   config.gem "errornot_notifier",   :version => "1.0.2"
   config.gem "google_analytics",    :version => "1.1.5", :lib => "rubaidh/google_analytics"
