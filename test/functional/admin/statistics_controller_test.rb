2c11edaa1377f75d3bd55bb7b6ad9e4b00d198d0
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2010-07-09 08:15:22 +0000

    Fix for back office (remove statistics).
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@307 d0c80317-126d-4d13-a798-19a31160aa05

--- test/functional/admin/statistics_controller_test.rb
+++ test/functional/admin/statistics_controller_test.rb
@@ -6,20 +6,17 @@ class Admin::StatisticsControllerTest < ActionController::TestCase
   fixtures(:typus_users)
 
   context "an user logged in" do
-    setup do    
+    setup do
       @request.session[:typus_user_id] = typus_users(:admin).id
     end
-    
+
     context "requesting GET :index" do
       setup do
         get :index
       end
-      
+
       should_respond_with(:success)
       should_render_template("index")
-      
-      should_assign_to(:nb_nl_invited)
-      should_assign_to(:nb_bc_used)
     end
   end
 end
