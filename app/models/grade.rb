eb7125d41bb69152e1569925bf3f9bd7b3134d23
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2011-02-24 18:09:21 +0000

    Add all missings grades (need better visual integration).
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@454 d0c80317-126d-4d13-a798-19a31160aa05

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -11,20 +11,25 @@ class Grade < ActiveRecord::Base
   GRADE_CATEGORY = {
     'Officier' => 1,
     'Sous-officier' => 2,
-    'Homme du rang' => 3
+    'Homme du rang' => 3,
+    'Médecin' => 4,
+    'Infirmier' => 5
   }.freeze
 
   GRADE = {
-    'Médecin capitaine' => 16,  'Infirmier' => 15,
+    'Médecin capitaine' => 19, 'Médecin commandant' => 20, 'Médecin lieutenant-colonel' => 21, 'Médecin colonel' => 22,
+    'Infirmier' => 15, 'Infirmier principal' => 16, 'Infirmier chef' => 17, "Infirmier d'encadrement" => 18,
+    'Colonel' => 14, 'Lieutenant-colonel' => 13, 'Commandant' => 12,
     'Capitaine' => 11, 'Lieutenant' => 10, 'Major' => 9,
-    # 'Colonel' => 14, 'Lieutenant-colonel' => 13, 'Commandant' => 12,
     'Adjudant-chef' => 8, 'Adjudant' => 7, 'Sergent-chef' => 6, 'Sergent' => 5,
     'Caporal-chef' => 4, 'Caporal' => 3, '1ère classe' => 2, '2e classe' => 1
   }.freeze
 
   GRADE_CATEGORY_MATCH = { 1 => 3, 2 => 3, 3 => 3, 4 => 3,
                            5 => 2, 6 => 2, 7 => 2, 8 => 2,
-                           9 => 1, 10 => 1, 11 => 1, 15 => 1, 16 => 1 } # 12 => 1, 13 => 1, 14 => 1,
+                           9 => 1, 10 => 1, 11 => 1, 12 => 1, 13 => 1, 14 => 1, 15 => 1, 16 => 1,
+                           15 => 5, 17 => 5, 18 => 5, 19 => 5,
+                           16 => 4, 20 => 4, 21 => 4, 22 => 4 }
 
   def self.new_defaults
     GRADE.sort_by {|grade| 1-grade[1] }.inject([]) do |result, grade|
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100714191820) do
+ActiveRecord::Schema.define(:version => 20110223220756) do
 
   create_table "check_lists", :force => true do |t|
     t.string   "title"
