aeb773b1908157290b2420e7ca6b7e10c3611196
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2011-03-06 22:17:44 +0000

    And fix the test to calm the cerberus
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@471 d0c80317-126d-4d13-a798-19a31160aa05

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
-class ApplicationController < Actionontroller::Base
+class ApplicationController < ActionController::Base
   protect_from_forgery
 
   helper_method :current_user_session, :current_user
