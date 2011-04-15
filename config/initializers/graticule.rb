0167947bcb557e810396d50d68a7535e1ab6de0a
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2011-04-15 13:40:15 +0000

    Monkey patch graticule to avoid encoding errors
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@521 d0c80317-126d-4d13-a798-19a31160aa05

--- config/initializers/graticule.rb
+++ config/initializers/graticule.rb
@@ -1,2 +1,28 @@
 # -*- encoding : utf-8 -*-
 Geocode.geocoder = Graticule.service(:google).new(GOOGLE_APPLICATION_ID)
+
+# Monkey patch Google geocoder to avoid UTF-8 encoding problems
+
+module Graticule
+  module Geocoder
+    class Google < Base
+
+      private
+
+      def parse_response(response)
+        result = response.placemarks.first
+        Location.new(
+          :latitude    => result.latitude,
+          :longitude   => result.longitude,
+          :street      => "", # ugly hack
+          :locality    => result.locality,
+          :region      => result.region,
+          :postal_code => result.postal_code,
+          :country     => result.country,
+          :precision   => result.precision
+        )
+      end
+
+    end
+  end
+end
