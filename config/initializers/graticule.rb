19b89057ca939a7b989a64fa55cf56cd2a0be5e9
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2011-08-07 16:12:08 +0000

    Upgrade a bunch of gems (and remove useless monkey patch for graticule)
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@571 d0c80317-126d-4d13-a798-19a31160aa05

--- config/initializers/graticule.rb
+++ config/initializers/graticule.rb
@@ -1,28 +1,2 @@
 # -*- encoding : utf-8 -*-
 Geocode.geocoder = Graticule.service(:google).new(GOOGLE_APPLICATION_ID)
-
-# Monkey patch Google geocoder to avoid UTF-8 encoding problems
-
-module Graticule
-  module Geocoder
-    class Google < Base
-
-      private
-
-      def parse_response(response)
-        result = response.placemarks.first
-        Location.new(
-          :latitude    => result.latitude,
-          :longitude   => result.longitude,
-          :street      => "", # ugly hack
-          :locality    => "",
-          :region      => "",
-          :postal_code => "",
-          :country     => "",
-          :precision   => result.precision
-        )
-      end
-
-    end
-  end
-end
