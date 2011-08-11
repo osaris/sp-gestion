310db46a328d2aa89e4337c681e6963f773f7638
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2011-08-11 21:12:54 +0000

    Don't repeat yourself (thx to @hirogeek)
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@573 d0c80317-126d-4d13-a798-19a31160aa05

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,10 @@
 CarrierWave.configure do |config|
   config.s3_access_key_id = "AKIAI3DSGZI62YJTITAQ"
   config.s3_secret_access_key = "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"
+  config.s3_cnamed = true  
   config.s3_bucket = "s3.sp-gestion.fr"
 
   config.s3_access_policy = :public_read
-
-  config.s3_cnamed = true
-  config.s3_bucket = "s3.sp-gestion.fr"
 
   config.s3_region = 'eu-west-1'
 end
