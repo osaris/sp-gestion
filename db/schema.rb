e8bae60ff0b41f8adaf93a48de2b06eddba7236d
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2011-08-24 20:15:33 +0000

    Add regimental number to firemen
    git-svn-id: http://code.imagineapp.com/svn/imagineapp/sp-gestion/trunk@576 d0c80317-126d-4d13-a798-19a31160aa05

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110818153553) do
+ActiveRecord::Schema.define(:version => 20110823210735) do
 
   create_table "check_lists", :force => true do |t|
     t.string   "title"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(:version => 20110818153553) do
     t.string   "cached_tag_list"
     t.string   "email"
     t.string   "passeport_photo"
+    t.string   "regimental_number"
   end
 
   add_index "firemen", ["station_id"], :name => "index_firemen_on_station_id"
