6efd30cd77d446ad6fd316fcdc46fbb0836bdc82
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2014-01-09 12:34:41 +0100

    Use new passenger method to restart app.

--- config/deploy.sample.rb
+++ config/deploy.sample.rb
@@ -32,7 +32,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
+      execute "sudo passenger-config restart-app #{release_path}"
     end
   end
 end
