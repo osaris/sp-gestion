445ee277bbf394f6fcc003c2bdeedd0b1c70170a
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2014-01-11 10:33:44 +0100

    Authorizations for dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,7 @@ class DashboardController < BackController
   helper(:interventions)
 
   def index
+    # authorization is handled at view level
     @convocations = @station.convocations.newer.limit(5)
     @interventions = @station.interventions.newer.limit(5)
     @items = Item.expirings(30, @station.id).limit(5)
