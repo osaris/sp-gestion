e0bf4e743b99ea240be7a953f5c0de3d4ee4dd7f
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2014-01-14 17:54:15 +0100

    Add spec for groups helper.

---
+++ spec/helpers/groups_helper_spec.rb
@@ -0,0 +1,24 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe GroupsHelper do
+
+  describe "#permission_icon" do
+
+    subject { permission_icon(authorized) }
+
+    context "authorized" do
+
+      let(:authorized) { true }
+
+      it { should == "<i class=\"icon-ok\"></i>" }
+    end
+
+    context "not authorized" do
+
+      let(:authorized) { false }
+
+      it { should == "<i class=\"icon-remove\"></i>" }
+    end
+  end
+end
