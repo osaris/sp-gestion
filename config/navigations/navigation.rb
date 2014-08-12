8dfdb4fa547ed03023be26d5efb6a7958e0baa2c
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2014-08-12 17:30:00 +0200

    Change layout to have menu fully deployed all the time (better UI for user rights).

--- config/navigations/navigation.rb
+++ config/navigations/navigation.rb
@@ -29,18 +29,23 @@ SimpleNavigation::Configuration.run do |navigation|
     # url - the address that the generated item links to. You can also use url_helpers (named routes, restful routes helper, url_for etc.)
     # options - can be used to specify attributes that will be included in the rendered navigation item (e.g. id, class etc.)
     #
-    primary.item(:home, 'Accueil', root_back_path) do |home|
+    primary.item(:user_account, icon_label_text('glyphicon glyphicon-info-sign', current_user.email)) do |user_account|
+      user_account.item(:logout, 'Déconnexion', logout_path)
+      user_account.item(:account, 'Mon compte', profile_path, :highlights_on => /^\/account/)
+    end
+
+    primary.item(:home, icon_label_text('glyphicon glyphicon-home', 'Accueil')) do |home|
       home.item(:dashboard, 'Le standard', root_back_path, :highlights_on => /^\/$/)
       home.item(:messages, 'Messages', messages_path, :highlights_on => /^\/messages/)
     end
 
-    primary.item(:admin, 'Administration', edit_account_path, :if => Proc.new { current_user.admin? }) do |admin|
+    primary.item(:admin, icon_label_text('glyphicon glyphicon-cog', 'Administration'), :if => Proc.new { current_user.admin? }) do |admin|
       admin.item(:account, 'Compte', edit_account_path, :highlights_on => /^\/account/)
       admin.item(:users, 'Utilisateurs', users_path, :highlights_on => /^\/users/)
       admin.item(:groups, 'Groupes', groups_path, :highlights_on => /^\/groups/)
     end
 
-    primary.item(:personnel, 'Personnel', firemen_path) do |personnel|
+    primary.item(:personnel, icon_label_text('glyphicon glyphicon-user', 'Personnel')) do |personnel|
       personnel.item(:firemen, 'Hommes', firemen_path, :highlights_on => /^\/firemen\/?((\d).*|new|facebook|trainings|\?page=(\d).*)?$/)
       personnel.item(:convocations, 'Convocations', convocations_path, :highlights_on => /^\/convocations/)
 
@@ -49,14 +54,14 @@ SimpleNavigation::Configuration.run do |navigation|
       personnel.item(:uniforms, 'Tenues', uniforms_path, :highlights_on => /^\/uniforms/)
     end
 
-    primary.item(:materiel, 'Matériel', check_lists_path) do |materiel|
+    primary.item(:materiel, icon_label_text('glyphicon glyphicon-list', 'Matériel')) do |materiel|
       materiel.item(:check_lists, 'Listes', check_lists_path, :highlights_on => /^\/check_lists/)
       materiel.item(:expirings, 'Expiration', expirings_items_path, :highlights_on => /^\/items\/expirings/)
 
       materiel.item(:vehicles, 'Véhicules', vehicles_path, :highlights_on => /^\/vehicles/)
     end
 
-    primary.item(:intervention, 'Interventions', interventions_path) do |intervention|
+    primary.item(:intervention, icon_label_text('glyphicon glyphicon-fire', 'Interventions')) do |intervention|
       intervention.item(:interventions_list, 'Liste', interventions_path, :highlights_on => /^\/interventions\/?((\d).*|new|\?page=(\d).*)?$/)
       intervention.item(:interventions_stats, 'Statistiques', interventions_stats_path((@current_year || Date.today.year), "by_type"), :highlights_on => /^\/interventions\/stats/)
 
