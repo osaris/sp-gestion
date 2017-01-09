32c920e7aed90e8b9e25aba2e01f75782d22e3ff
Raphaël Emourgeon <raphael.emourgeon@gmail.com>
2017-01-09 21:53:21 +0100

    Use s3 URL for HTTPS compatibility.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ SpGestion::Application.configure do
 
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "//" + SPG_CONFIG[:fog][:directory]
+  config.action_controller.asset_host = "//" + SPG_CONFIG[:fog][:directory] + ".s3.amazonaws.com"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
